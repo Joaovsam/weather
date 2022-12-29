a932d001975deb6454f3a8186267eadea7cd8e9a
Joaovsam <joao.amorim@sydle.com>
2022-12-29 18:35:44 -0300

    adicionar sistema de clima

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { Grid, IconButton, Paper, TextField, Card, Typography } from "@material-ui/core";
+//import SearchIcon from "@mui/icons-material";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [currentWeatecCondition, setCurrentWeatecCondition] = useState();
+  const [autoComplete, setAutoComplete] = useState("Timoteo");
+  let cor = "#00FFFF"
+
+  //31277a23a4924344afc173640222912
+  function fillCurrentWeather() {
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=31277a23a4924344afc173640222912&q=${autoComplete}&lang=pt`
+    ).then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      }
+    }).then((data) => {
+      console.log(data)
+      setCurrentWeatecCondition(data);
+    })
+  }
+
+  function fillAutoComplete() {
+    fetch(
+      `http://api.weatherapi.com/v1/search.json?key=31277a23a4924344afc173640222912&&q=${autoComplete}&lang=pt`
+    ).then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      }
+    }).then((data) => {
+      console.log(data)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container direction="column" >
+
+
+      <Grid container>
+        <Paper>
+          <TextField
+            id="standard-basic"
+            label="Cidade"
+            variant="outlined"
+            value={autoComplete}
+            onChange={
+              (e) => {
+                fillAutoComplete();
+                setAutoComplete(e.target.value)
+              }}
+          />
+          <IconButton aria-label="searchCity" onClick={() => { fillCurrentWeather(); }}>
+            Pesquisa
+          </IconButton>
+        </Paper>
+      </Grid>
+
+      <Grid container>
+        {currentWeatecCondition ?
+          <Grid item style={{ width: "100vw" }}>
+            <Card style={{ height: "50vh" }}>
+              <Typography>{currentWeatecCondition.location.name}</Typography>
+              <Typography style={{ fontSize: 36, color: cor }}>{currentWeatecCondition.current.temp_c}º</Typography>
+              <Typography>{currentWeatecCondition.current.condition.text}</Typography>
+              <Typography>Sensação térmica: {currentWeatecCondition.current.feelslike_c}º</Typography>
+              <img src={currentWeatecCondition.current.condition.icon} />
+            </Card>
+          </Grid>
+          : <></>
+        }
+        <Grid>
+
+        </Grid>
+      </Grid>
+
+      <Grid container>
+        asdas
+      </Grid>
+
+    </Grid>
+
   );
 }
 
